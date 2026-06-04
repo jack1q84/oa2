ec995660849195a69596b268debba7e1c0fd37db
jack1q84 <59546093+jack1q84@users.noreply.github.com>
2026-06-04 16:41:12 +0800

    docs: 为 HelloWorld.java 添加文档注释

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+/**
+ * HelloWorld - Issue #1 功能实现
+ * 使用Java输出 hello world！
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world！");
